b9c4325dd75125385a897b2b54cc4f7c4e108777
rvandeghen <renaud.vandeghen@elr.be>
2019-12-13 13:13:32 +0100

    add Unet folders

---
+++ Part2/Codes/Unet/prediction.py
@@ -0,0 +1,50 @@
+from keras.models import load_model
+import numpy as np
+from PIL import Image
+import keras
+
+from unet import mean_iou
+
+import sys
+
+dependencies = {
+    'mean_iou': mean_iou
+}
+# Load the models
+model = load_model('unet_weights.39-0.01.h5')
+
+# Load the image
+image_inp = Image.open('../test/images/input/elps_eye_98.png')
+mask_inp = Image.open('../test/masks/output/mask_eye_98.png')
+# image_inp = Image.open('../NoEllipses/noelps_eye2015-01-16_02-46-45-005.png')
+
+# Scale the input between [0 1] and change the arrays (1, 512, 512, 3)
+image_inp = np.array(image_inp)/255.
+inp = np.expand_dims(image_inp, axis=0)
+
+# Predict the segmanted mask given the model
+y_pred = model.predict(inp)
+
+# Reshape the output (240, 320)
+y_pred = np.reshape(y_pred, (240, 320))
+
+# Set all pixels under a threshold at 0 and the other at 1
+# This threshold is computed manually 
+# y_pred[y_pred < 0.1] = 0
+# y_pred[y_pred >= 0.1] = 1
+
+# Add the segmantation to the input image
+# Red channel is used for the roads and the Green one for the buildings
+image_inp[..., 0] += y_pred
+
+image_inp[image_inp > 1] = 1
+
+# Transform the array in a image representation
+image_out = keras.preprocessing.image.array_to_img(image_inp)
+image_out.show()
+
+mask_inp.show()
+
+# Save the image
+# image_out.save("nice.png", format='png')
+
